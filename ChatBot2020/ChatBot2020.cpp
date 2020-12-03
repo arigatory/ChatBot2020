3363d2795397fab127927b0bee4107ba0d645cef
Иван Иван <remillius_dies@outlook.com>
2020-12-03 22:17:00 +0300

    regex add

--- ChatBot2020/ChatBot2020.cpp
+++ ChatBot2020/ChatBot2020.cpp
@@ -1,20 +1,34 @@
-// ChatBot2020.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <regex>
+using namespace std;
+
+void to_lower(string& str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello dear user, please ask your question!\n";
+    string question; //user question
+        while (question != "exit") {
+            getline(cin, question);
+            to_lower(question);
+
+
+            regex hello = regex(".*hello.*");
+            if (regex_match(question, hello))
+            {
+                cout << "Hello again my friend\n";
+            }
+
+            regex name = regex(".*what is your name.*");
+            if (regex_match(question, name))
+            {
+                cout << "My name is Bot2020\n";
+            }
+            
+        }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
